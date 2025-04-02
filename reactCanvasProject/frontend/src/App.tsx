d06e9ff5bb549ece7674160fdf4efe9ba73a92b8
Michael <mjtumiwa@gmail.com>
2025-04-03 02:39:00 +0800

    Dockerize backend and frontend app

---
+++ reactCanvasProject/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CanvasVideo from './CanvasVideo.tsx'
+import SearchImages from './SearchImages.tsx'
+import GraphQLImageSearch from './GQLSearchImages.tsx'
+
+function App() {
+  return (
+    <>
+      <CanvasVideo/>
+      <SearchImages/>
+      <GraphQLImageSearch/>
+    </>
+  )
+}
+
+export default App
